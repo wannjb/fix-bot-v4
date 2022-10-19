ee1d1842404410bfe48ad671954580717b0763cc
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-19 14:27:39 +0800

    Update _autoreact.js

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -2,15 +2,13 @@ let handler = async (m, { conn, text }) => {
 
     conn.sendMessage(m.chat, {
           react: {
-            text: `${pickRandom(['😨','😅','😂','😳','😎', '🥵', '😱', '🐦', '🙄', '🐤','🗿','🐦','🤨','🥴','😐','👆','😔', '👀','👎'])}`,
+            text: `${pickRandom(['🙂','😅','😂','👀','😎', '🤪', '😐', '😅', '🙄'])}`,
             key: m.key,
           }})
   
   }
-handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k|owner)/i
+handler.customPrefix = /(ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k|owner)/i
 handler.command = new RegExp
-  
-handler.mods = false
 
 export default handler
 

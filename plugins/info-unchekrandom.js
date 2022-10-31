315d8f781fd0c09bd3d9489694421f4073ef25b2
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-31 20:17:49 +0800

    Update info-unchekrandom.js

--- plugins/info-unchekrandom.js
+++ plugins/info-unchekrandom.js
@@ -26,7 +26,7 @@ Ayo di pollow 💪😼
 ┆⟥⟤ ➠ (${global.bottime})
 └─┈⟅`
 let nth = `-----❨ *RANDOM UNCHEK!* ❩-----`
-conn.send3ButtonImg(m.chat, `${pickRandom(flaaa2)}` + `${ucapan()} ` + `${name}`, nth, rules, 'FF', '.nsfw neko', 'ML', '.nsfw hentai', 'Owner', '.owner', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.send3ButtonImg(m.chat, `${pickRandom(flaaa2)}` + `${ucapan()} ` + `${name}`, nth, rules, 'FF', '.nsfw neko', 'ML', '.nsfw oppai', 'Owner', '.owner', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: 'https://wa.me//62895327999292',
     mediaType: 2, 
     description: sgc,

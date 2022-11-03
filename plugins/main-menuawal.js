1a01066b700fc0babc1d887554fdd210021d2112
wannjb <113450108+wannjb@users.noreply.github.com>
2022-11-03 21:17:30 +0800

    Update main-menuawal.js

--- plugins/main-menuawal.js
+++ plugins/main-menuawal.js
@@ -75,7 +75,6 @@ ${readMore}
 ⮕ ${chats.length - groupsIn.length} - Personal Chats
 ⮕ ${chats.length} - Total Chats
 `
-let ᴛᴇs = `Pᴏᴡᴇʀ Bʏ ⬝ @${nomorwa.split`@`[0]}\nCʀᴇᴀᴛᴏʀ Bᴏᴛ ⬝ @${nomorown1.split`@`[0]}\n⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 await conn.sendButtonDoc(m.chat, cap, botdate, 'Owner', usedPrefix + 'owner', fkontak, {
 			contextInfo: {
 				forwardingScore: fsizedoc,

